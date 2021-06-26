b875c406e2a1728d56aa4b394f35edfaed754425
SonVu1601 <67354093+SonVu1601@users.noreply.github.com>
2021-06-26 11:59:12 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,62 @@
+<div class="footer">
+		<!-- footer fist section -->
+		<section class="fist-footer">
+			<div class="container-footer">
+				<div class="row">					
+					<div class="item freeship">
+						<i class="fas fa-dolly"></i>
+						<div class="content freeship">
+							<h3>Miễn phí vận chuyển</h3>
+							<p>Cho đơn hàng trên 1000k</p>
+						</div>
+					</div>						
+					<div class="item transport-fast">
+						<i class="far fa-star"></i>
+						<div class="content transport">
+							<h3>Cam kết chính hãng</h3>
+							<p>Đảm bảo chất lượng 100%</p>
+						</div>
+					</div>					
+					<div class="item help">
+						<i class="far fa-smile-wink"></i>
+						<div class="content help">
+							<h3>Hỗ trợ tư vấn</h3>
+							<p>Thứ 2-Thứ 7 : 08h30 - 17h30</p>
+						</div>
+					</div>
+					<div class="item order-now">
+						<i class="fas fa-phone-alt"></i>
+						<div class="content help">
+							<h3>190016868</h3>
+							<p>Đặt hàng ngay</p>
+						</div>
+					</div>							
+				</div>
+			</div>	
+		</section>
+		<!-- footer fist section -->
+		<section class="second-footer">
+			<div class="container-footer">
+				<div class="row">
+					<div class="address">
+						<p><strong>Sữa Tăng Cân Tăng Cơ Cho Người Gầy, Tập Gym Chính Hãng và Chất Lượng</strong></p>
+						<p><strong>Phù Hợp Cho Người Bắt Đầu Tập Gym và Không Có Thời Gian Ăn Uống ...</strong></p>
+						<P>Cơ sở : Số 175 Tây Sơn - Đống Đa - Hà Nội </p>
+						<p>Hotline: 19001868 - 19006666- Zalo:012356546</p>
+						<p>Đóng góp ý kiến và phản hồi vào email dưới đây !</p>
+						<p>Email: goldengym@gmail</p>
+						<p>
+							<br/>
+							<img alt="dathongbao" src="images/dathongbao.png">
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/scroll.js"></script>
+</body>
+</html>
---
+++ submitsuasp.php
@@ -0,0 +1,50 @@
+<?php
+    session_start();
+    include 'config/config1.php';
+    $id = $_GET["id"];
+    if(isset($_POST["them"])){
+        $id_dm = $_POST['iddm'];
+        $tensp = $_POST['tensp'];
+        $khoiluong = $_POST['khoiluong'];
+        $ngaysx = $_POST['ngaysx'];
+        $hansd = $_POST['hansd'];
+        $giact = $_POST['giact'];
+        $giakm = $_POST['giakm'];
+        $thuonghieu = $_POST['thuonghieu'];
+        $image = $_FILES['image']['name'];
+        $doituongsd = $_POST['doituongsd'];
+        $mota = $_POST['mota'];
+       
+       
+        $image_tmp = $_FILES['image']['tmp_name']; 
+
+        $uploadOk = 1;
+        $sql = "UPDATE sanpham SET id_dm='$id_dm',tensp='$tensp',khoiluong='$khoiluong',hansd='$ngaysx',hansd='$hansd',giact='$giact',giakm='$giakm',thuonghieu='$thuonghieu',anhsp='$image',doituongsd='$doituongsd',motasp='$mota' WHERE id =$id";
+        $target_dir = "images/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+       
+        
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+
+                $_SESSION["thongbao"]= "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                header("location:suasanpham.php?id=$id");
+                die();
+                
+        } else {
+
+            mysqli_query(mysqli_connect($servername, $dbusername,$dbpass, $dbname ),$sql);
+            move_uploaded_file($image_tmp,'images/'.$image);
+            $_SESSION["thongbao"]="Sửa sản phẩm Thành Công";
+            header("location:sanphamadmin.php");
+            die();
+        }
+        
+    
+    
+    
+        
+
+     }
+?>
